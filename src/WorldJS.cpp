f35fcd1b74641d4606f50b5c074707b2aff6bfea
740291272 <740291272@qq.com>
2019-07-10 11:49:52 +0800

    fix: undefined

--- src/WorldJS.cpp
+++ src/WorldJS.cpp
@@ -20,11 +20,12 @@ extern "C" {
         return GetAudioLength(files);
     }
 
-    void DisplayInformation(int fs, int nbit, int x_length) {
+    int DisplayInformation(int fs, int nbit, int x_length) {
         printf("File information\n");
         printf("Sampling : %d Hz %d Bit\n", fs, nbit);
         printf("Length %d [sample]\n", x_length);
         printf("Length %f [sec]\n", static_cast<double>(x_length) / fs);
+        return 0;
     }
 }
 
