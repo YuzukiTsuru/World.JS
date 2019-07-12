732f27ae63ff47a2f03d0ef82c074901d626c1be
740291272 <740291272@qq.com>
2019-07-12 23:11:44 +0800

    add a example and dio function

--- src/WorldJS.cpp
+++ src/WorldJS.cpp
@@ -47,6 +47,55 @@ val Get2XArray(Type **arr, int y_len, int x_len)
     }
     return arr2x;
 }
+template <class Type>
+Type *GetPtrFrom1XArray(val arr, int *len = NULL)
+{
+    if (len == NULL)
+    {
+        len = new int[1];
+    }
+    *len = arr["length"].as<int>();
+    Type *ret = new Type[*len];
+    val module = val::global("Module");
+    int ptr = (int)ret / sizeof(Type);
+    module["HEAPF64"].call<val>("set", arr, val(ptr));
+    return ret;
+}
+
+template <class Type>
+Type **GetPtrFrom2XArray(val arr, int *y_len = NULL, int *x_len = NULL)
+{
+    if (y_len == NULL)
+    {
+        x_len = new int[1];
+    }
+    if (x_len == NULL)
+    {
+        x_len = new int[1];
+    }
+
+    *y_len = arr["length"].as<int>();
+
+    val module = val::global("Module");
+    int ptr;
+    if (*y_len > 0)
+    {
+        *x_len = arr[0]["length"].as<int>();
+        Type **ret = new Type *[*y_len];
+        for (int i = 0; i < *y_len; i++)
+        {
+            ret[i] = new Type[*x_len];
+            ptr = (int)ret[i] / sizeof(Type);
+            module["HEAPF64"].call<val>("set", arr[i], val(ptr));
+        }
+        return ret;
+    }
+    else
+    {
+        *x_len = 0;
+        return NULL;
+    }
+}
 } // namespace
 
 int DisplayInformation(int fs, int nbit, int x_length)
@@ -60,12 +109,17 @@ int DisplayInformation(int fs, int nbit, int x_length)
 // WavFile Read
 val WavRead_JS(string filename)
 {
+    // init val 
     val InWav = val::object();
+    // Get File Name
     const char *f = filename.c_str();
     int fs, nbit;
+    // GetAudioLength for read
     int x_length = GetAudioLength(f);
     double *x = new double[x_length];
+    // Use tools/audioio.cpp wavread function
     wavread(f, &fs, &nbit, x);
+    // Set the output value
     InWav.set("x", Get1XArray<double>(x, x_length));
     InWav.set("fs", fs);
     InWav.set("nbit", nbit);
@@ -74,9 +128,40 @@ val WavRead_JS(string filename)
     return InWav;
 }
 
+val Dio_JS(val x_val, int fs, double frame_period)
+{
+    // init val 
+    val ret = val::object();
+    int x_length;
+    // translate array to C++ ptr
+    double *x = GetPtrFrom1XArray<double>(x_val, &x_length);
+    // init dio
+    DioOption option = {0};
+    InitializeDioOption(&option);
+    // Get dio settings
+    option.frame_period = frame_period;
+    option.speed = 1;
+    option.f0_floor = 71.0;
+    option.allowed_range = 0.2;
+    // Get Samples For DIO
+    int f0_length = GetSamplesForDIO(fs, x_length, frame_period);
+    double *f0 = new double[f0_length];
+    double *time_axis = new double[f0_length];
+    // run dio
+    Dio(x, x_length, fs, &option, time_axis, f0);
+    // Set the output value
+    ret.set("f0", Get1XArray<double>(f0, f0_length));
+    ret.set("time_axis", Get1XArray<double>(time_axis, f0_length));
+    // destory memory
+    delete[] f0;
+    delete[] time_axis;
+    delete[] x;
+    return ret;
+}
+
 EMSCRIPTEN_BINDINGS(my_module)
 {
     emscripten::function("DisplayInformation", &DisplayInformation);
     emscripten::function("WavRead_JS", &WavRead_JS);
+    emscripten::function("Dio_JS", &Dio_JS);
 }
-
---
+++ src/WorldJS/WorldJS.h
@@ -0,0 +1,12 @@
+//-----------------------------------------------------------------------------
+// WORLD core functions.
+// Note: win.sln uses an option in Additional Include Directories.
+//-----------------------------------------------------------------------------
+#include "world/d4c.h"
+#include "world/dio.h"
+#include "world/harvest.h"
+#include "world/matlabfunctions.h"
+#include "world/cheaptrick.h"
+#include "world/stonemask.h"
+#include "world/synthesis.h"
+#include "world/synthesisrealtime.h"
