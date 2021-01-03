927b48deadfb62f058e39af63349f9c65e8d0f82
YuzukiTsuru <gloomyghost@gloomyghost.com>
2021-01-03 23:46:03 +0800

    init Wav2World Library

---
+++ src/Wav2World/Wav2World.cpp
@@ -0,0 +1,9 @@
+//
+// Created by YuzukiTsuru on 2021/1/3.
+//
+
+#include <iostream>
+
+#include "WorldJS.h"
+
+#include "Wav2World.h"
---
+++ src/Wav2World/Wav2World.h
@@ -0,0 +1,14 @@
+//
+// Created by YuzukiTsuru on 2021/1/3.
+//
+
+#ifndef WORLDJS_WAV2WORLD_H
+#define WORLDJS_WAV2WORLD_H
+
+
+class Wav2World {
+
+};
+
+
+#endif //WORLDJS_WAV2WORLD_H
