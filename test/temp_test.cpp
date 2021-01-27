f990dbc1edff11317162c6df6b42d3b1f5b6c8b1
YuzukiTsuru <gloomyghost@gloomyghost.com>
2021-01-27 15:18:50 +0800

    finish Wav2World

--- test/temp_test.cpp
+++ test/temp_test.cpp
@@ -3,9 +3,23 @@
 //
 
 #include <iostream>
+
 #include "Wav2World.h"
+#include <audioio.h>
 
 
 int main() {
-    std::string file = "vaiueo2d.wav"
+    std::string file = "../test/vaiueo2d.wav";
+    int x_length = GetAudioLength(file.c_str());
+    if (x_length <= 0) {
+        if (x_length == 0) printf("error: File not found.\n");
+        else printf("error: The file is not .wav format.\n");
+        return -1;
+    }
+    auto *x = new double[x_length];
+    // wavread() must be called after GetAudioLength().
+    int fs, nbit;
+    wavread(file.c_str(), &fs, &nbit, x);
+    Wav2World(x, x_length, fs);
+    return 0;
 }
